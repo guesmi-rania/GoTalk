cc2db6da6cff0760ea62a4f8bcfd607a8913b3db
rania.guesmi <rania.guesmi@esen.tn>
2026-01-07 07:50:45 +0000

    Connect GoTalk to Render Postgres

--- pkg/database/db.go
+++ pkg/database/db.go
@@ -1,18 +1,26 @@
 package database
 
 import (
-    "fmt"
-    "gorm.io/driver/postgres"
-    "gorm.io/gorm"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func Connect() {
-    dsn := "host=localhost user=postgres password=postgres dbname=gotalk port=5432 sslmode=disable"
-    var err error
-    DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    if err != nil {
-        panic(fmt.Sprintf("failed to connect database: %v", err))
-    }
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("DATABASE_URL not set")
+	}
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect database:", err)
+	}
+
+	DB = db
+	log.Println("Database connected")
 }
